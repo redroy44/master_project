0a61128af2343656e9e767f34e5b2ced0c1a2344
piobandu <redroy44@gmail.com>
2014-10-05 23:52:14 +0200

    added NoiseEstimator class. First commit

---
+++ NoiseEstimator.cpp
@@ -0,0 +1,18 @@
+/*
+ * NoiseEstimator.cpp
+ *
+ *  Created on: 5 paź 2014
+ *      Author: piotrek
+ */
+
+#include <NoiseEstimator.h>
+
+NoiseEstimator::NoiseEstimator() {
+	// TODO Auto-generated constructor stub
+
+}
+
+NoiseEstimator::~NoiseEstimator() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ NoiseEstimator.h
@@ -0,0 +1,19 @@
+/*
+ * NoiseEstimator.h
+ *
+ *  Created on: 5 paź 2014
+ *      Author: piotrek
+ */
+
+#ifndef NOISEESTIMATOR_H_
+#define NOISEESTIMATOR_H_
+
+#include <Wave.h>
+
+class NoiseEstimator: public Wave {
+public:
+	NoiseEstimator();
+	virtual ~NoiseEstimator();
+};
+
+#endif /* NOISEESTIMATOR_H_ */
